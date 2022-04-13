62a674d1366fcde997e708d0af9518b72bc953c0
czbell <cztoptian@163.com>
2022-04-13 11:05:09 +0800

    addquery

--- StudentDemo/Pages/Index.cshtml.cs
+++ StudentDemo/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace StudentDemo.Pages;
 
 public class IndexModel : PageModel
 {
+
+    public Dictionary<string, string> Query { get; set; } = new Dictionary<string, string>();
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +17,10 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        foreach(var kv in Request.Headers)
+        {
+            Query.Add(kv.Key, kv.Value);
+        }
 
     }
 }
